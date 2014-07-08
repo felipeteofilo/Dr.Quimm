bb2e58945481e755b831c8f7f55235a2872db38d
Julia <julia.a.c.yamamoto@gmail.com>
2014-07-08 15:23:47 -0300

    Criada classe de NPC
    Com o pior nome possível

---
+++ DrQuim/DQnpc.h
@@ -0,0 +1,13 @@
+//
+//  DQnpc.h
+//  DrQuim
+//
+//  Created by Julia Yamamoto on 08/07/14.
+//  Copyright (c) 2014 LEONARDO DE SOUSA MENDES. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface DQnpc : SKNode
+
+@end
---
+++ DrQuim/DQnpc.m
@@ -0,0 +1,13 @@
+//
+//  DQnpc.m
+//  DrQuim
+//
+//  Created by Julia Yamamoto on 08/07/14.
+//  Copyright (c) 2014 LEONARDO DE SOUSA MENDES. All rights reserved.
+//
+
+#import "DQnpc.h"
+
+@implementation DQnpc
+
+@end
