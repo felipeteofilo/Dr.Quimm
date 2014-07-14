8dfc54fe7929be54d84bc914eda4cfb3dccd36b3
Julia <julia.a.c.yamamoto@gmail.com>
2014-07-14 13:54:47 -0300

    Revert 107ef13..647e0b9
    This rolls back to commit 107ef137465d425544ac74e39d1f9fd6867d5f8c.

--- DrQuim/DQnpc.m
+++ DrQuim/DQnpc.m
@@ -10,4 +10,35 @@
 
 @implementation DQnpc
 
+-(id)initComNome:(NSString *)nome
+{
+    self = [super self];
+    if(self){
+        self.nome = nome;
+        
+        //Configura o dicionarioDeFalas dependendo do nome atribuído a ele
+    
+    }
+    return self;
+}
+
+-(void)criarSpriteNodeComNome:(NSString*)nome naPosicao:(CGPoint)posicao{
+    
+    //Inicia o sprite node TEMPORÁRIO
+    self.spriteNode = [SKSpriteNode spriteNodeWithColor:[UIColor purpleColor] size:CGSizeMake(100, 100)];
+    
+    //Configura o SpriteNode
+    [self.spriteNode setAnchorPoint:CGPointMake(0, 0)];
+    [self.spriteNode setName:nome];
+    [self.spriteNode setPosition:posicao];
+    [self.spriteNode setZPosition:0];
+}
+
+-(void)interagir
+{
+    //Verifica se está em alguma missão
+        //Se sim -> Verifica a parte e o que deve dizer ou dar
+        //Se não -> verifica o respeito e o que dizer dependendo dele.
+}
+
 @end
