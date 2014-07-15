836d3e95874aa83082845b09b29b1deabbda21ef
Julia <julia.a.c.yamamoto@gmail.com>
2014-07-15 20:07:14 -0300

    Feito tutorial do início
    Estou pensando em melhorá-lo um pouco

--- DrQuim/DQFlorestaParte1.m
+++ DrQuim/DQFlorestaParte1.m
@@ -16,8 +16,6 @@
 //Metodo que inicia a cena
 -(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
-        
-        
         self.controleCutscenes = [[DQCutsceneControle alloc]initComParte:1 Fase:1];
         
         self.cutsceneEstaRodando = YES;
@@ -35,8 +33,11 @@
     
     [self definirPontosRadiacao];
     
-    self.cutsceneEstaRodando=NO;
-    self.estaFalando=NO;
+    self.cutsceneEstaRodando = NO;
+    self.estaFalando = NO;
+    
+    self.mostrouTutorial = NO;
+    self.executandoTutorial = NO;
     
 }
 //Ultimo Método que é chamado antes de aparecer a tela, usado para arrumar a camera //===OK===
@@ -49,31 +50,38 @@
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
     
-    
     //Se não está falando e nem em cutscene...
     if (!self.cutsceneEstaRodando && !self.estaFalando) {
         
-        //Verifica em qual lado da tela o jogador está tocando
-        UITouch *posicao = [touches anyObject];
-        
-        
-        //Jeito porco irei arrumar
-        CGPoint posicaoDoToque = [posicao locationInNode:self];
-        SKSpriteNode *nodeTocado = (SKSpriteNode *)[self nodeAtPoint:posicaoDoToque];
-        
-        if ([[nodeTocado name] isEqualToString:@"RadiacaoAlfa"]) {
-            [self.controleCutscenes mostrarFalaNoJogo:self KeyDaFala:@"RadiacaoAlfa"];
-            
-            self.cutsceneEstaRodando = YES;
-            self.estaFalando = YES;
+        //Se ainda não mostrou o tutorial, assim que a pessoa clica na tela, ele inicia:
+        if(!self.mostrouTutorial){
+            if(!self.executandoTutorial){
+                [self iniciarTutorial];
+            }
         }
-        else if ([[nodeTocado name] isEqualToString:@"RadiacaoBeta"]) {
-            [self.controleCutscenes mostrarFalaNoJogo:self KeyDaFala:@"RadiacaoBeta"];
+        
+        else{
+            //Verifica em qual lado da tela o jogador está tocando
+            UITouch *posicao = [touches anyObject];
+        
+            //Jeito porco irei arrumar
+            CGPoint posicaoDoToque = [posicao locationInNode:self];
+            SKSpriteNode *nodeTocado = (SKSpriteNode *)[self nodeAtPoint:posicaoDoToque];
+        
+            if ([[nodeTocado name] isEqualToString:@"RadiacaoAlfa"]) {
+                [self.controleCutscenes mostrarFalaNoJogo:self KeyDaFala:@"RadiacaoAlfa"];
             
-            self.cutsceneEstaRodando = YES;
-            self.estaFalando = YES;
-        }else{
-            [super touchesBegan:touches withEvent:event];
+                self.cutsceneEstaRodando = YES;
+                self.estaFalando = YES;
+            }
+            else if ([[nodeTocado name] isEqualToString:@"RadiacaoBeta"]) {
+                [self.controleCutscenes mostrarFalaNoJogo:self KeyDaFala:@"RadiacaoBeta"];
+            
+                self.cutsceneEstaRodando = YES;
+                self.estaFalando = YES;
+            }else{
+                [super touchesBegan:touches withEvent:event];
+            }
         }
     }
     
@@ -84,17 +92,82 @@
             
         }
         else{
-            
             self.estaFalando = NO;
             self.cutsceneEstaRodando = NO;
         }
     }
 }
 
+-(void)iniciarTutorial
+{
+    self.executandoTutorial = YES;
+    [self.jogador pararAndar];
+    
+    //Cria o SpriteNode de tutorial
+    //Pular
+    SKSpriteNode *tutorialPular = [[SKSpriteNode alloc]init];
+    [tutorialPular setSize:CGSizeMake(self.frame.size.width/2, self.frame.size.height)];
+    [tutorialPular setAnchorPoint:CGPointMake(0, 0)];
+    [tutorialPular setPosition:CGPointMake(0, 0)];
+    //Correr
+    SKSpriteNode *tutorialCorrer = [[SKSpriteNode alloc]init];
+    [tutorialCorrer setSize:CGSizeMake(self.frame.size.width/2, self.frame.size.height)];
+    [tutorialCorrer setAnchorPoint:CGPointMake(0, 0)];
+    [tutorialCorrer setPosition:CGPointMake(self.frame.origin.x + self.frame.size.width/2, 0)];
+    
+    //Arrumando coisas para ação do tutorial de pular
+        //Pega imagens
+    SKTextureAtlas *atlasPular = [SKTextureAtlas atlasNamed:@"tutorialPular"];
+    SKTexture *AP1 = [atlasPular textureNamed:@"tutorialPular001.png"];
+    SKTexture *AP2 = [atlasPular textureNamed:@"tutorialPular002.png"];
+    SKTexture *AP3 = [atlasPular textureNamed:@"tutorialPular003.png"];
+    SKTexture *AP4 = [atlasPular textureNamed:@"tutorialPular004.png"];
+        //Adiciona num array
+    NSArray *arrayTutorialPular = @[AP1, AP2, AP3, AP4];
+        //Cria a ação
+    SKAction *acaoTutorialPular = [SKAction animateWithTextures:arrayTutorialPular timePerFrame:0.4f];
+    
+    //Arrumando coisas para ação do tutorial de correr
+    //Pega imagens
+    SKTextureAtlas *atlasCorrer = [SKTextureAtlas atlasNamed:@"tutorialCorrer"];
+    SKTexture *AC1 = [atlasCorrer textureNamed:@"tutorialCorrer001.png"];
+    SKTexture *AC2 = [atlasCorrer textureNamed:@"tutorialCorrer002.png"];
+    SKTexture *AC3 = [atlasCorrer textureNamed:@"tutorialCorrer003.png"];
+    SKTexture *AC4 = [atlasCorrer textureNamed:@"tutorialCorrer004.png"];
+    SKTexture *AC5 = [atlasCorrer textureNamed:@"tutorialCorrer005.png"];
+    SKTexture *AC6 = [atlasCorrer textureNamed:@"tutorialCorrer006.png"];
+    SKTexture *AC7 = [atlasCorrer textureNamed:@"tutorialCorrer007.png"];
+    SKTexture *AC8 = [atlasCorrer textureNamed:@"tutorialCorrer008.png"];
+    SKTexture *AC9 = [atlasCorrer textureNamed:@"tutorialCorrer009.png"];
+    SKTexture *AC10 = [atlasCorrer textureNamed:@"tutorialCorrer010.png"];
+    SKTexture *AC11 = [atlasCorrer textureNamed:@"tutorialCorrer011.png"];
+    SKTexture *AC12 = [atlasCorrer textureNamed:@"tutorialCorrer012.png"];
+    SKTexture *AC13 = [atlasCorrer textureNamed:@"tutorialCorrer013.png"];
+    //Adiciona num array
+    NSArray *arrayTutorialCorrer = @[AC1, AC2, AC3, AC4, AC5, AC6, AC7, AC8, AC9, AC10, AC11, AC12, AC13];
+    //Cria a ação
+    SKAction *acaoTutorialCorrer = [SKAction animateWithTextures:arrayTutorialCorrer timePerFrame:0.35f];
+    
+
+    //Fazendo o tutorial de pular
+    [self addChild:tutorialPular];
+    [tutorialPular runAction:acaoTutorialPular completion:^{
+        [self.jogador pular];
+        [tutorialPular removeFromParent];
+        
+        //Fazendo tutorial de correr
+        [self addChild:tutorialCorrer];
+        [tutorialCorrer runAction:acaoTutorialCorrer completion:^{
+            [tutorialCorrer removeFromParent];
+        }];
+    }];
+    
+    self.mostrouTutorial = YES;
+}
+
 //Metodo chamado toda hora pela spriteKit, usado para criar as partes do corpo fisico da fase ==OK==
 -(void)update:(NSTimeInterval)currentTime{
     if (!self.cutsceneEstaRodando) {
-        
         [super update:currentTime];
         [self procurarRadiacao];
         
