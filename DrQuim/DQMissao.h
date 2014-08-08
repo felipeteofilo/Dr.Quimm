0baa41140ea8a2dfe22099913aeb1a6e89cec64e
FELIPE TEOFILO SOUZA SANTOS <felipe.teofilo@hotmail.com>
2014-08-08 17:41:21 -0300

    Terminada a classe DQMissao
    So falta testar

--- DrQuim/DQMissao.h
+++ DrQuim/DQMissao.h
@@ -10,16 +10,30 @@
 
 @interface DQMissao : NSObject
 
+
+//Variaveis do tipo NSString que guardam as informacoes da missao
 @property NSString *nome;
 @property NSString *disparador;
 @property NSString *recompensa;
-@property int respeito;
 @property NSArray *arrayPartes;
-@property int quantidadeDePartes;
 @property NSString *ID;
 
+
+//Variavel do tipo int que guarda o quanto de respeito sera guanho na missao
+@property int respeito;
+
+//Varialvel do tipo int que guarda a quantidade de partes essa missao terá
+@property int quantidadeDePartes;
+
+
+//Metodo para iniciar uma missao passando qual o numero da missao
 -(id)initMissao :(int)missao;
--(Boolean)podePassarComNPC :(NSString*)nomeNPC Item:(NSString*)item;
+
+
+//Metodo que retorna um boolean informando se pode ser passada a parte atual ou ja esta no fim da missao
+-(Boolean)podePassarComNPC :(NSString*)nomeNPC Item:(NSString*)item Parte:(int)parte;
+
+//Metodo que retorna um boolean informando se pode iniciar a missao ou nao
 -(Boolean)podeIniciarComNPC :(NSString*)nomeNPC;
 
 @end
