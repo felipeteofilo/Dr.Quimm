1309c5e2db136b18e039bc920d4ea995cb5d48cb
Leonardo de Sousa Mendes <lleo.sm@gmail.com>
2014-08-08 17:47:04 -0300

    DQControleUserDefaults
    Inserido novos metodos

--- DrQuim/DQControleUserDefalts.h
+++ DrQuim/DQControleUserDefalts.h
@@ -11,13 +11,23 @@
 @interface DQControleUserDefalts : NSObject
 
 +(NSUserDefaults*)userDefalts;
+
 +(float)volumeMusica;
 +(float)volumeSons;
 +(BOOL)sonsMudo;
 +(BOOL)musicaMuda;
++(NSDictionary*)estadosJogador;
++(int)faseAtual;
++(int)parteFaseAtual;
++(NSString*)missaoAtualJogador;
+
 +(void)setVolumeMusica:(float)volume;
 +(void)setVolumeSons:(float)volume;
 +(void)setMusicaMuda:(BOOL)mudo;
 +(void)setSonsMudo:(BOOL)mudo;
++(void)setEstadoJogadorVida:(float)vida Fome:(float)fome Sede:(float)sede;
++(void)setFaseAtual:(int)fase;
++(void)setParteFaseAtual:(int)parte;
++(void)setMissaoAtualJogador:(NSString*)missaoAtual;
 
 @end
--- DrQuim/DQControleUserDefalts.m
+++ DrQuim/DQControleUserDefalts.m
@@ -30,6 +30,28 @@
     return [[self userDefalts]boolForKey:@"musicaMuda"];
 }
 
++(NSDictionary*)estadosJogador{
+    NSMutableDictionary *estadosJogador=[NSMutableDictionary dictionary];
+    
+    [estadosJogador setObject:[NSNumber numberWithFloat:[[self userDefalts]floatForKey:@"Vida"]] forKey:@"Vida"];
+    [estadosJogador setObject:[NSNumber numberWithFloat:[[self userDefalts]floatForKey:@"Fome"]] forKey:@"Fome"];
+    [estadosJogador setObject:[NSNumber numberWithFloat:[[self userDefalts]floatForKey:@"Sede"]] forKey:@"Sede"];
+    
+    return estadosJogador;
+}
+
++(int)faseAtual{
+   return [[self userDefalts]integerForKey:@"FaseAtual"];
+}
+
++(int)parteFaseAtual{
+    return [[self userDefalts]integerForKey:@"parteAtual"];
+}
++(NSString*)missaoAtualJogador{
+    return [[self userDefalts]objectForKey:@"MissaoJogador"];
+}
+
+
 +(void)setVolumeMusica:(float)volume{
     [[self userDefalts]setFloat:volume forKey:@"volumeMusica"];
 }
@@ -45,5 +67,19 @@
 +(void)setSonsMudo:(BOOL)mudo{
     [[self userDefalts]setBool:mudo forKey:@"somMudo"];
 }
++(void)setEstadoJogadorVida:(float)vida Fome:(float)fome Sede:(float)sede{
+    [[self userDefalts]setFloat:vida forKey:@"Vida"];
+        [[self userDefalts]setFloat:fome forKey:@"Fome"];
+        [[self userDefalts]setFloat:sede forKey:@"Sede"];
+}
++(void)setFaseAtual:(int)fase{
+    [[self userDefalts]setInteger:fase forKey:@"FaseAtual"];
+}
++(void)setParteFaseAtual:(int)parte{
+    [[self userDefalts]setInteger:parte forKey:@"ParteAtual"];
+}
 
++(void)setMissaoAtualJogador:(NSString*)missaoAtual{
+    [[self userDefalts]setObject:missaoAtual forKey:@"MissaoJogador"];
+}
 @end
