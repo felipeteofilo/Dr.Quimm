e73cfe5f950a4c5552aef84154db987a0651795d
Leonardo de Sousa Mendes <lleo.sm@gmail.com>
2014-08-11 17:15:08 -0300

    DQContadorGeiger
    Criado Prototipo classe

---
+++ DrQuim/DQContadorGeiger.h
@@ -0,0 +1,16 @@
+//
+//  DQContadorGeiger.h
+//  DrQuim
+//
+//  Created by LEONARDO DE SOUSA MENDES on 11/08/14.
+//  Copyright (c) 2014 LEONARDO DE SOUSA MENDES. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface DQContadorGeiger : SKSpriteNode
+
+@property NSString *nomeSomAlerta;
+@property int nivelPerigo;
+
+@end
---
+++ DrQuim/DQContadorGeiger.m
@@ -0,0 +1,13 @@
+//
+//  DQContadorGeiger.m
+//  DrQuim
+//
+//  Created by LEONARDO DE SOUSA MENDES on 11/08/14.
+//  Copyright (c) 2014 LEONARDO DE SOUSA MENDES. All rights reserved.
+//
+
+#import "DQContadorGeiger.h"
+
+@implementation DQContadorGeiger
+
+@end
