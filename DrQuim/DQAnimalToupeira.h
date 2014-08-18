b02a495e4c09e439c2f1f0a8d0ee7d9931827480
Leonardo de Sousa Mendes <lleo.sm@gmail.com>
2014-08-17 22:25:42 -0300

    DQAnimal
    Implantada as classes de animais

---
+++ DrQuim/DQAnimalToupeira.h
@@ -0,0 +1,17 @@
+//
+//  DQAnimalToupeira.h
+//  criacaoAnimal
+//
+//  Created by LEONARDO DE SOUSA MENDES on 15/08/14.
+//  Copyright (c) 2014 Leonardo de Sousa Mendes. All rights reserved.
+//
+
+#import "DQAnimal.h"
+#import "DQUteis.h"
+
+@interface DQAnimalToupeira : DQAnimal
+
+@property float distanciaCorrer;
+
+-(id)initToupeira;
+@end
---
+++ DrQuim/DQIsca.m
@@ -0,0 +1,20 @@
+//
+//  DQIsca.m
+//  criacaoAnimal
+//
+//  Created by Leonardo de Sousa Mendes on 04/08/14.
+//  Copyright (c) 2014 Leonardo de Sousa Mendes. All rights reserved.
+//
+
+#import "DQIsca.h"
+
+@implementation DQIsca
+
+-(id)initIsca{
+    if (self=[super init]) {
+        self.objeto=@"liquido";
+        self.detalhe=@"amarelo";
+    }
+    return self;
+}
+@end
