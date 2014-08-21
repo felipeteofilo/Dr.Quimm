65f9e615bb5fed45f998424325dfcf64349b5cc8
FELIPE TEOFILO SOUZA SANTOS <felipe.teofilo@hotmail.com>
2014-08-21 17:26:50 -0300

    Arrumando a quest 2
    estou testando para ver ele troca de fase

--- DrQuim/DQMissaoControle.m
+++ DrQuim/DQMissaoControle.m
@@ -16,13 +16,13 @@
         self.cena = cenaRecebida;
         
         //Inicia na parte 0
-        self.parteAtual = 7;
+        self.parteAtual = 0;
         
         //E sem nenhuma missão
         self.emMissao = NO;
         
         //Define a próxima missão (a primeira)
-        self.proximaMissao = 1;
+        self.proximaMissao = 2;
         
         //Apresentando a próxima missão (no caso, a primeira)
         self.missao = [[DQMissao alloc] initMissao:self.proximaMissao];
