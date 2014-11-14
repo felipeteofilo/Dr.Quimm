985d4ca9969dbd8dde1c262a343af1868fd9d1a1
Julia Yamamoto <julia.a.c.yamamoto@gmail.com>
2014-11-14 18:10:32 -0200

    iniciando interação com o projeto principal

---
+++ DrQuim/DQcirculoComposto.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+DQcirculoComposto.m
+//  DrQuim
+//
+//  Created by JULIA ARISSA COPPOLA YAMAMOTO on 14/11/14.
+//  Copyright (c) 2014 LEONARDO DE SOUSA MENDES. All rights reserved.
+//
+
+#import "DQcirculoComposto.h"
+
+@implementation DQcirculoComposto
+
+@end
---
+++ DrQuim/DQcirculoGiratorioProtocolo.h
@@ -0,0 +1,15 @@
+//
+//  DQcirculoGiratorio.h
+//  DrQuim
+//
+//  Created by JULIA ARISSA COPPOLA YAMAMOTO on 14/11/14.
+//  Copyright (c) 2014 LEONARDO DE SOUSA MENDES. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol DQcirculoGiratorioProtocolo <NSObject>
+
+-(void)mudouValor: (NSString *)novoValor;
+
+@end
